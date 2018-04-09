c2c02b67cfcce6392502db3addeba5a00d6abf80
A.Sofiane Guerfi <asofianeguerfi@ASofianes-MacBook-Pro.local>
2018-04-09 15:55:00 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import ReactDom from 'react-dom';
 import Button from 'material-ui/Button'
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { browserHistory, BrowserRouter as Router, Route, } from 'react-router';
+import {  BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 import HomePage from './components/HomePage.jsx';
@@ -16,6 +16,7 @@ import Header from './components/Header.jsx';
 class App extends Component {
   render() {
     return (
+    <Router>
         <div className='container'>
           <div className="row">
               <div className='col-xs-10 col-xs-offset-1'>
@@ -24,13 +25,33 @@ class App extends Component {
           </div>
           <div className="row">
               <div className='col-xs-10 col-xs-offset-1'>
-                  <HomePage/>
+                   <Route exact path="/" component={HomePage} />
+                   <Route exact path="/header" component={Header} />
+                  
               </div>
           </div>
-          <Button variant="raised" color="primary">
-             Hello World
-          </Button>
+          <hr/>
+          <div className="row">
+              <div className='col-xs-10 col-xs-offset-1'>
+                <Button variant="raised" color="primary">
+                    Hello World
+                </Button>
+              </div>
+          </div>
+          
         </div>
+    </Router>
+
+        //****   <Router>
+//     <div className ="container">
+//     
+//     <Route exact path="/" component={Home} />
+//     <Route exact path="/about" component={About} />
+//     <Route exact path="/Search" component={Search} />
+//     <Route path="/contact" component={Contact} />
+//   </div>
+// </Router>
+// */
 
       
     );
